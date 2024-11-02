967be3773f713041002359463650cd08e30c4069
Stiven <strujillodev@gmail.com>
2024-11-02 13:55:11 -0500

    feat: server

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+// import { corsConfig } from "./config/cors";
+// import { connectDB } from "./config/db";
+// import bookRoutes from "./routes/bookRoutes";
+
+dotenv.config();
+
+// Conexión a la base de datos
+// connectDB();
+
+const app = express();
+
+// Middlewares Globales
+// app.use(cors(corsConfig)); // Habilitar CORS
+app.use(morgan("dev")); // Logger
+app.use(express.json()); // Leer datos de formularios
+
+// Routes
+// app.use("/api/books", bookRoutes);
+
+export default app;
